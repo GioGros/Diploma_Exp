45fc60f95be8808b1465a2e834f5a2405c0c26b3
GioGros <87187049+GioGros@users.noreply.github.com>
2024-03-28 22:08:25 +0300

    Add files via upload

---
+++ Diploma1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 0;
+    int z;
+
+    // Деление на ноль
+    z = x / y;
+
+    // Неиспользуемая переменная
+    int unused_variable;
+
+    // Операция сравнения вместо присваивания
+    if (x = 5) {
+        printf("x is 5\n");
+    }
+
+    return 0;
+}
