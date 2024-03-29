4b3335b062232dde52e7ca2f5a325724583077c5
GioGros <87187049+GioGros@users.noreply.github.com>
2024-03-29 20:50:18 +0300

    Add files via upload

---
+++ Diploma2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define x 5
+
+int main() {
+    int y = 0;
+    int z;
+
+    z = x / y;
+
+    int unused_variable;
+
+    if (x == 5) {  
+        printf("x is 5\n");
+    }
+
+    return 0;
+}
